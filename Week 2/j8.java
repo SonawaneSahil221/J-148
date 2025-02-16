b038f7e14fc1ccbd4eff22068c61139783d6a9f0
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-02-17 01:49:18 +0530

    Create j8.java

---
+++ Week 2/j8.java
@@ -0,0 +1,12 @@
+public class StudentGrades {
+    public static void main(String[] args) {
+        int marks = 85;
+        char grade;
+        if (marks >= 90) grade = 'A';
+        else if (marks >= 80) grade = 'B';
+        else if (marks >= 70) grade = 'C';
+        else if (marks >= 60) grade = 'D';
+        else grade = 'F';
+        System.out.println("Grade: " + grade);
+    }
+}
