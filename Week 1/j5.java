9dd0adf918bcd92f8f62b8ff93db4a13f1a17661
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-02-17 01:37:59 +0530

    Create j5.java

---
+++ Week 1/j5.java
@@ -0,0 +1,7 @@
+public class RectangleArea {
+    public static void main(String[] args) {
+        int length = 5, width = 10;
+        int area = length * width;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
