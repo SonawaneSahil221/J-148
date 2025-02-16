453d5e1508bb50d4c17ca8daf0fa8ea78e630662
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-02-17 01:39:34 +0530

    Create j8.java

---
+++ Week 1/j8.java
@@ -0,0 +1,19 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int n = 100; // Change this to any value for a custom range
+        System.out.println("Prime numbers between 1 and " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
