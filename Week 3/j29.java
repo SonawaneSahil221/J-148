da6d87ddb39e60687cf2d26e944da91e1f7c31c9
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:39:54 +0530

    Update j29.java

--- Week 3/j29.java
+++ Week 3/j29.java
@@ -1,1 +1,14 @@
+public class RemoveAdjacentDuplicates {
+    public static void main(String[] args) {
+        String str = "aaabbcddd";
+        StringBuilder result = new StringBuilder();
 
+        for (char c : str.toCharArray()) {
+            if (result.length() == 0 || result.charAt(result.length() - 1) != c) {
+                result.append(c);
+            }
+        }
+
+        System.out.println("String after removing adjacent duplicates: " + result);
+    }
+}
