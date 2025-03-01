ec9bd471222812bdd1db179ff915ffb06b716c2f
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:39:02 +0530

    Update j26.java

--- Week 3/j26.java
+++ Week 3/j26.java
@@ -1,1 +1,19 @@
+public class FindTwoLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 30, 25};
+        int highest = Integer.MIN_VALUE, secondHighest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            } else if (num > secondHighest && num != highest) {
+                secondHighest = num;
+            }
+        }
+
+        System.out.println("Highest: " + highest);
+        System.out.println("Second Highest: " + secondHighest);
+    }
+}
 
