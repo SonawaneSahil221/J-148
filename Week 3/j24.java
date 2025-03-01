a42f5d84abd0b7da1b4b3b8166debbca36095ce1
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:38:31 +0530

    Update j24.java

--- Week 3/j24.java
+++ Week 3/j24.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class AddMatrices {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1, 2}, {3, 4}};
+        int[][] matrix2 = {{5, 6}, {7, 8}};
+        int[][] sum = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        System.out.println("Sum of matrices:");
+        for (int[] row : sum) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 
