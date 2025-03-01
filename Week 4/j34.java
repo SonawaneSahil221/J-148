36626ae141a9244b0a8e30ec5eae6f93057a094b
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:12:10 +0530

    Update j34.java

--- Week 4/j34.java
+++ Week 4/j34.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class CountDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter number N: ");
+        String N = scanner.next();
+        
+        System.out.print("Enter digit D: ");
+        char D = scanner.next().charAt(0);
+        
+        int count = 0;
+        for (char c : N.toCharArray()) {
+            if (c == D) {
+                count++;
+            }
+        }
+        
+        System.out.println("Total count of digit " + D + " occurring in " + N + " is: " + count);
+        scanner.close();
+    }
+}
 
