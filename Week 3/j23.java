ee8b47dad4ae39a19a17e41334861cfa09957cc2
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:37:40 +0530

    Update j23.java

--- Week 3/j23.java
+++ Week 3/j23.java
@@ -1,1 +1,8 @@
+public class CharArrayToString {
+    public static void main(String[] args) {
+        char[] charArray = {'H', 'e', 'l', 'l', 'o'};
+        String str = new String(charArray);
+        System.out.println("String: " + str);
+    }
+}
 
