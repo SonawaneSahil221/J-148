7152395bbc59635fd9bd24104f8220b59a9d3381
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:39:18 +0530

    Update j27.java

--- Week 3/j27.java
+++ Week 3/j27.java
@@ -1,1 +1,14 @@
+import java.util.Arrays;
 
+public class ConcatenateArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {4, 5, 6};
+        int[] result = new int[arr1.length + arr2.length];
+
+        System.arraycopy(arr1, 0, result, 0, arr1.length);
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+
+        System.out.println("Concatenated array: " + Arrays.toString(result));
+    }
+}
