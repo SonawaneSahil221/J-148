63607638f30a4f8d55cef1bbf8994ef2024f6bd2
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:58:37 +0530

    Delete Week 3/j32.java

--- Week 3/j32.java
+++
@@ -1,7 +0,0 @@
-public class ReplaceDigits {
-    public static void main(String[] args) {
-        String str = "H3ll0 W0rld";
-        String result = str.replaceAll("[0-9]", "*");
-        System.out.println("Modified string: " + result);
-    }
-}
