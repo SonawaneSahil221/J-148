c1d3ff815b74ade8cd11b07c616e24f188ad4c19
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:12:51 +0530

    Update j35.java

--- Week 4/j35.java
+++ Week 4/j35.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class ToggleString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String str = scanner.next();
+        
+        StringBuilder toggled = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (Character.isUpperCase(ch))
+                toggled.append(Character.toLowerCase(ch));
+            else
+                toggled.append(Character.toUpperCase(ch));
+        }
+        
+        System.out.println("Toggled string: " + toggled);
+        scanner.close();
+    }
+}
 
