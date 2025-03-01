c730be655fc6578bb2a1974e1a1ec9244475538b
SonawaneSahil221 <141671385+SonawaneSahil221@users.noreply.github.com>
2025-03-01 23:38:47 +0530

    Update j25.java

--- Week 3/j25.java
+++ Week 3/j25.java
@@ -1,1 +1,10 @@
+import java.util.Arrays;
+
+public class SortStrings {
+    public static void main(String[] args) {
+        String[] words = {"banana", "apple", "cherry", "date"};
+        Arrays.sort(words);
+        System.out.println("Sorted words: " + Arrays.toString(words));
+    }
+}
 
